2c3ca91eb4e39e1a0f7fa82f78b82622bf6fff5e
AnkushDiverseCoder <thakurankushsingh1902@gmail.com>
2023-05-14 14:53:33 +0530

    Complete Error Solved

--- frontend/src/api/API.js
+++ frontend/src/api/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://tender-worm-fatigues.cyclic.app/api"
+const baseUrl = "https://busy-jade-lemming-hat.cyclic.app/api"
 
 const api = axios.create({
   baseURL: baseUrl,
